af965bf08872568f844005dd7c31252351df1a99
heerabn <heerachaithra@gmail.com>
2026-03-12 11:18:14 +0530

    adding files

---
+++ src/Day33/task1.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 12 11:14:23 2026
+
+@author: heera
+"""
+
+import numpy as np
+
+point1 = np.array([2, 3])
+point2 = np.array([5, 7])
+
+# Euclidean Distance
+euclidean = np.linalg.norm(point1 - point2)
+
+# Manhattan Distance
+manhattan = np.sum(np.abs(point1 - point2))
+
+print(f"Point 1: {point1}")
+print(f"Point 2: {point2}")
+print(f"Euclidean: {euclidean:.4f}")
+print(f"Manhattan: {manhattan}")
